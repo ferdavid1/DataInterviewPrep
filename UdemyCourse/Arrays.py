c237a1d8a4158b64d460e24e6e38a6f3ec58185b
ferdavid1 <fespinosa@ucdavis.edu>
2017-08-20 04:07:46 -0700

    arrays

---
+++ UdemyCourse/Arrays.py
@@ -0,0 +1,20 @@
+# 'array sequence' is the general term
+# Python has 3 main sequence classes:
+	# list: [1,2,3]
+	# tuple: (1,2,3)
+	# string: '123'
+# all support indexing (e.g. t[0]=1)
+
+'''
+How computers store information
+	Focus on computer memory
+	Units of memory (bits and bytes)
+	Memory retrieval 
+'''
+
+'''
+Memory of a computer is stored in bits
+typical unit of byte, which is 8 bits
+Computers typicallu use a memory address 
+Each byte is associated with unique address.
+	Byte #2144 vs Byte #2147
