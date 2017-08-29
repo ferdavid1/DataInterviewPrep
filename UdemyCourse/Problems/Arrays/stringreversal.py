d2c1880547911f8b1557f101fa8929e358c8d1db
ferdavid1 <fespinosa@ucdavis.edu>
2017-08-29 12:32:18 -0700

    grid search 1

--- UdemyCourse/Problems/Arrays/stringreversal.py
+++ UdemyCourse/Problems/Arrays/stringreversal.py
@@ -7,25 +7,16 @@ list = list[::-1]
 RUN THIS CELL TO TEST YOUR SOLUTION
 """
 def rev_word(s):
-	s = s.strip().replace("    ", ",").replace(",", " ")
-	words = []
-	for ind, x in enumerate(s[:-2]):
-		if not x.isalpha():
-			if s[ind+1].isalpha():
-				words.append(s[:ind+1:])
-				words.append(s[ind+1:])
-			else:
-				pass
-	print(words)
-
+	print(s.split()[::-1])
+	return " ".join(s.split()[::-1])
 
 from nose.tools import assert_equal
 
 class ReversalTest(object):
     
     def test(self,sol):
-        # assert_equal(sol('    space before'),'before space')
-        # assert_equal(sol('space after     '),'after space')
+        assert_equal(sol('    space before'),'before space')
+        assert_equal(sol('space after     '),'after space')
         assert_equal(sol('   Hello John    how are you   '),'you are how John Hello')
         assert_equal(sol('1'),'1')
         print ("ALL TEST CASES PASSED")
