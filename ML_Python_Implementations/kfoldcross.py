4e06267b1989c6b6ab9d8c3326cfe041ac60782e
ferdavid1 <fespinosa@ucdavis.edu>
2017-08-29 12:13:50 -0700

    k fold

---
+++ ML_Python_Implementations/kfoldcross.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn import cross_validation
+from sklearn import datasets
+from sklearn import svm
+diabets = datasets.load_diabetes()
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(diabets.data, diabets.target, test_size=0.2, random_state=0)
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+clf = svm.SVC(kernel='linear', C=1)
+scores = cross_validation.cross_val_score(clf, diabets.data, diabets.target, cv=4) # 4-folds
+print(scores)
+print("Mean: {}. \nScore: {}".format(scores.mean(), scores.std()))
+
