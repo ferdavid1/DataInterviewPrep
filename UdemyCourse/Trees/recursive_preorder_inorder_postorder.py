e55560fe96fd56a918e85d1b76b1d555fe4ff8a5
ferdavid1 <fespinosa@ucdavis.edu>
2017-09-24 14:31:12 -0700

    recursion in trees

--- UdemyCourse/Trees/recursive_preorder_inorder_postorder.py
+++ UdemyCourse/Trees/recursive_preorder_inorder_postorder.py
@@ -2,6 +2,17 @@
 def preorder(tree): # root, then left, then right
 	if tree: # base case
 		print(tree.getRootVal())
-		preorder(tree.getLeftChild)
-		preorder(tree.getRightChild)
+		preorder(tree.getLeftChild())
+		preorder(tree.getRightChild())
 
+def inorder(tree):
+	if tree:
+		inorder(tree.getLeftChild())
+		print(tree.getRootVal())
+		inorder(tree.getRightChild())
+
+def postorder(tree):
+	if tree:
+		inorder(tree.getLeftChild())
+		inorder(tree.getRightChild())
+		print(tree.getRootVal())
